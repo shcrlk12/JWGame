3541706ad4415f057f1a0e080777d195149bd0c4
kjwon12 <kjwon12@picopack.co.kr>
2025-05-07 15:17:17 +0900

    first commit

---
+++ Assets/Scripts/FirstPersonController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class FirstPersonController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.collider.tag);
+    }
+}
